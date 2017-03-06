88549c7ae7b716ff1041596116494cb84be528d6
Redeemus <demesurejerry@sfr.fr>
2017-03-06 14:38:22 +0100

    Recup function

--- app/webroot/testrecup.php
+++ app/webroot/testrecup.php
@@ -6,11 +6,28 @@
  * and open the template in the editor.
  */
 
+$j=3;
+$m='janvier';
+$a=2016;
+
 $doc = new DOMDocument;
-$doc->loadHTMLFile('http://www.infoclimat.fr/observations-meteo/archives/4/mars/2017/rennes-st-jacques/07130.html');
-$tableHeaders = $doc->getElementsByTagName('td');
-foreach ($tableHeaders as $th)
+$doc->loadHTMLFile('http://www.infoclimat.fr/observations-meteo/archives/'.$j.'/'.$m.'/'.$a.'/rennes-st-jacques/07130.html');
+$tableElements = $doc->getElementsByTagName('td');
+$nbElements = $tableElements->length;
+for ($i = 0; $i <$nbElements-15; $i=$i+10)
 {
-    echo $th->nodeValue . '<br />';
+    echo $tableElements->item($i)->nodeValue . '<br />';
 }
 
+
+for ($i = 7; $i <$nbElements-15; $i=$i+10)
+{
+    echo $tableElements->item($i)->nodeValue . '<br />';
+}
+
+for ($i = 4; $i <$nbElements-15; $i=$i+10)
+{
+    echo $tableElements->item($i)->nodeValue . '<br />';
+}
+
+echo $tableElements->item(247)->nodeValue . '<br />';
